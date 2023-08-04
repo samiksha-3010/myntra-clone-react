c51ff3cd5096c0ecbd313c90f2dd4fd5205eff00
samiksha-3010 <samiksha30102@gmail.com>
2023-08-04 17:18:47 +0530

    update

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Component/Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h2>helllllo</h2>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+      <Routes>
+      <Route exact path='/'element={<Home/>}/>
+
+      </Routes>
     </div>
   );
 }
